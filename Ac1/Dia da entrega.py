f0d87827915cc79045a5fa20c6e73329a4a5813c
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2022-07-23 23:12:18 -0300

    Create Dia da entrega.py

---
+++ Ac1/Dia da entrega.py
@@ -0,0 +1,106 @@
+dia_semana = input()
+dia_mes = int(input())
+if dia_semana == 'domingo' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'domingo' and dia_mes == 1:
+    print('sera entregue segunda')
+elif dia_semana == 'domingo' and dia_mes == 2:
+    print('sera entregue terca')
+elif dia_semana == 'domingo' and dia_mes == 3:
+    print('sera entregue quarta')
+elif dia_semana == 'domingo' and dia_mes == 4:
+    print('sera entregue quinta')
+elif dia_semana == 'domingo' and dia_mes == 5:
+    print('sera entregue sexta')
+elif dia_semana == 'domingo' and dia_mes == 6:
+    print('sera entregue sabado')
+
+if dia_semana == 'segunda' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'segunda' and dia_mes == 1:
+    print('sera entregue terca')
+elif dia_semana == 'domingo' and dia_mes == 2:
+    print('sera entregue quarta')
+elif dia_semana == 'segunda' and dia_mes == 3:
+    print('sera entregue quinta')
+elif dia_semana == 'segunda' and dia_mes == 4:
+    print('sera entregue sexta')
+elif dia_semana == 'segunda' and dia_mes == 5:
+    print('sera entregue sabado')
+elif dia_semana == 'segunda' and dia_mes == 6:
+    print('sera entregue domingo')
+
+if dia_semana == 'terca' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'terca' and dia_mes == 1:
+    print('sera entregue quarta')
+elif dia_semana == 'terca' and dia_mes == 2:
+    print('sera entregue quinta')
+elif dia_semana == 'terca' and dia_mes == 3:
+    print('sera entregue sexta')
+elif dia_semana == 'terca' and dia_mes == 4:
+    print('sera entregue sabado')
+elif dia_semana == 'terca' and dia_mes == 5:
+    print('sera entregue domingo')
+elif dia_semana == 'terca' and dia_mes == 6:
+    print('sera entregue segunda')
+
+if dia_semana == 'quarta' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'quarta' and dia_mes == 1:
+    print('sera entregue quinta')
+elif dia_semana == 'quarta' and dia_mes == 2:
+    print('sera entregue sexta')
+elif dia_semana == 'quarta' and dia_mes == 3:
+    print('sera entregue sabado')
+elif dia_semana == 'quarta' and dia_mes == 4:
+    print('sera entregue domingo')
+elif dia_semana == 'quarta' and dia_mes == 5:
+    print('sera entregue segunda')
+elif dia_semana == 'quarta' and dia_mes == 6:
+    print('sera entregue terca')
+
+if dia_semana == 'quinta' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'quinta' and dia_mes == 1:
+    print('sera entregue sexta')
+elif dia_semana == 'quinta' and dia_mes == 2:
+    print('sera entregue sabado')
+elif dia_semana == 'quinta' and dia_mes == 3:
+    print('sera entregue domingo')
+elif dia_semana == 'quinta' and dia_mes == 4:
+    print('sera entregue segunda')
+elif dia_semana == 'quinta' and dia_mes == 5:
+    print('sera entregue terca')
+elif dia_semana == 'quinta' and dia_mes == 6:
+    print('sera entregue quarta')
+
+if dia_semana == 'sexta' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'sexta' and dia_mes == 1:
+    print('sera entregue sabado')
+elif dia_semana == 'sexta' and dia_mes == 2:
+    print('sera entregue domingo')
+elif dia_semana == 'sexta' and dia_mes == 3:
+    print('sera entregue segunda')
+elif dia_semana == 'sexta' and dia_mes == 4:
+    print('sera entregue terca')
+elif dia_semana == 'sexta' and dia_mes == 5:
+    print('sera entregue quarta')
+elif dia_semana == 'sexta' and dia_mes == 6:
+    print('sera entregue quinta')
+
+if dia_semana == 'sabado' and dia_mes == 0:
+    print('chega hoje!')
+elif dia_semana == 'sabado' and dia_mes == 1:
+    print('sera entregue domingo')
+elif dia_semana == 'sabado' and dia_mes == 2:
+    print('sera entregue segunda')
+elif dia_semana == 'sabado' and dia_mes == 3:
+    print('sera entregue terca')
+elif dia_semana == 'sabado' and dia_mes == 4:
+    print('sera entregue quarta')
+elif dia_semana == 'sabado' and dia_mes == 5:
+    print('sera entregue quinta')
+elif dia_semana == 'sabado' and dia_mes == 6:
+    print('sera entregue sexta')
