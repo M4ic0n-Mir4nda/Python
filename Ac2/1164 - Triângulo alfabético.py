2657e3ca88414583d4ed0cb3d3c8d8c6c2a904e1
M4ic0n-Mir4nda <maiconmirandasantana@gmail.com>
2022-07-23 23:12:26 -0300

    Create 1164 - Triângulo alfabético.py

---
+++ Ac2/1164 - Triângulo alfabético.py
@@ -0,0 +1,12 @@
+n = int(input())
+qtd = 0
+alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+if 1 <= n <= 26:
+    for i in range(1, n + 1):
+        for j in range(i):
+            if qtd == n:
+                qtd -= 1
+            print(alfabeto[qtd], end='')
+        qtd += 1
+        print()
