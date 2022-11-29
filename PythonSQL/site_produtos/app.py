123aceca6432411122ed7666d71aba0ebb18a3fc
M4ic0n-Mir4nda <107010440+M4ic0n-Mir4nda@users.noreply.github.com>
2022-11-28 21:33:47 -0300

    Update

---
+++ PythonSQL/site_produtos/app.py
@@ -0,0 +1,62 @@
+import pyodbc
+from flask import Flask, render_template, redirect, url_for, request
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=DESKTOP-E9ARPQB;"
+    "Database=PythonSQL;"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+print('Conexão bem Sucedida')
+
+cursor = conexao.cursor()
+
+app = Flask(__name__)
+
+class Produto:
+    def __init__(self, codigo, descricao, quantidade, valor):
+        self.codigo = codigo
+        self.descricao = descricao
+        self.quantidade = quantidade
+        self.valor = valor
+
+prod1 = Produto(12563, 'Arroz 5kg', 50, 15.22)
+prod2 = Produto(52369, 'Bolacha trakinas', 31, 2.99)
+lista_prod = [prod1, prod2]
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo='Produtos', produtos=lista_prod)
+
+@app.route('/estoque')
+def estoque():
+    return render_template('estoque.html')
+
+@app.route('/adicionar', methods=['GET', 'POST'])
+def adicionar():
+    if request.method == 'POST':
+        codigo = request.form['codigo']
+        descricao = request.form['descricao']
+        quantidade = request.form['quantidade']
+        valor = request.form['valor']
+        produto = Produto(codigo, descricao, quantidade, valor)
+        cadastro_prod = f"""
+            INSERT INTO PRODUTO (CODIGO, DESCRICAO, QUANTIDADE, VALOR)
+            VALUES
+            ('{codigo}', '{descricao}', {quantidade}, {valor})
+            """
+        cursor.execute(cadastro_prod)
+        cursor.commit()
+        lista_prod.append(produto)
+
+        '''
+        produto = Produto(
+            request.form['codigo'],
+            request.form['descricao'],
+            request.form['quantidade'],
+            request.form['valor'])
+        '''
+        return redirect(url_for('index'))
+
+app.run(debug=True)
